f1e0f8d4d6a15d8e7ff842016b029fec809a5656
Baleria <baleria@Balerias-MacBook.local>
2021-05-20 11:23:40 -0700

    Up to name input complete.

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 /*This is a sweet, text-based exploration of our solar system*/
 
+#include <stdio.h>
+
 int main(int argc, char** argv)  {
+    printf("Welcome to the Solar System!\n");
+    printf("There are 9 planets to explore.\n");
+    printf("What is youir name?\n");
+    char name[20];
+    scanf("%s", name);
+    printf("Nice to meet you, %s.\nMy name is Eliza. I'm an old friend of Alexa.\n", name);
     return 0; 
     }
